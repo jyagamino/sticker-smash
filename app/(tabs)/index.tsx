b22f975346793d1799f218a4c80993acb0a8e7e0
jyagamino <jyagamino@gmail.com>
2025-01-14 18:22:22 +0900

    絵文字ステッカーの操作機能を追加

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,6 +1,7 @@
-import { View, StyleSheet } from "react-native";
-import * as ImagePicker from "expo-image-picker";
 import { useState } from "react";
+import { View, StyleSheet } from "react-native";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import * as ImagePicker from "expo-image-picker";
 import { type ImageSource } from "expo-image";
 
 import ImageViewer from "@/components/ImageViewer";
@@ -55,7 +56,7 @@ export default function Index() {
   };
 
   return (
-    <View style={styles.container}>
+    <GestureHandlerRootView style={styles.container}>
       <View style={styles.imageContainer}>
         <ImageViewer
           imgSource={PlaceholderImage}
@@ -93,7 +94,7 @@ export default function Index() {
       <EmojiPicker isVisible={isModalVisible} onClose={onModalClose}>
         <EmojiList onSelect={setPickedEmoji} onCloseModal={onModalClose} />
       </EmojiPicker>
-    </View>
+    </GestureHandlerRootView>
   );
 }
 
